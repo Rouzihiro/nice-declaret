ca8ea505d4315d11578833ffc13550fbe1ad2461
0xravy <0xravy@gamil.com>
2024-02-22 19:54:52 +0300

    add vm

--- system/vm/default.nix
+++ system/vm/default.nix
@@ -2,35 +2,35 @@
 
 {
 
-  # Enable dconf (System Management Tool)
-  programs.dconf.enable = true;
+    # Enable dconf (System Management Tool)
+    programs.dconf.enable = true;
 
-  # Add user to libvirtd group
-  users.users.ravy.extraGroups = [ "libvirtd" ];
+    # Add user to libvirtd group
+    users.users.ravy.extraGroups = [ "libvirtd" ];
 
-  # Install necessary packages
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
-    spice spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    gnome.adwaita-icon-theme
-  ];
+    # Install necessary packages
+    environment.systemPackages = with pkgs; [
+        virt-manager
+        virt-viewer
+        spice spice-gtk
+        spice-protocol
+        win-virtio
+        win-spice
+        gnome.adwaita-icon-theme
+    ];
 
-  # Manage the virtualisation services
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-      };
+    # Manage the virtualisation services
+    virtualisation = {
+        libvirtd = {
+            enable = true;
+            qemu = {
+                swtpm.enable = true;
+                ovmf.enable = true;
+                ovmf.packages = [ pkgs.OVMFFull.fd ];
+            };
+        };
+        spiceUSBRedirection.enable = true;
     };
-    spiceUSBRedirection.enable = true;
-  };
-  services.spice-vdagentd.enable = true;
+    services.spice-vdagentd.enable = true;
 
 }
